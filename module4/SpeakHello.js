44299f8c7de9fffad4fe7c09ca82df5ec08c0a8d
Max Lutsenko <lytsenko.maksim@gmail.com>
2024-11-20 20:22:02 +0500

    module 4 upd3

--- module4/SpeakHello.js
+++ module4/SpeakHello.js
@@ -7,13 +7,14 @@
     // See Lecture 52, part 1
     // var helloSpeaker =
 
+    var helloSpeaker = {};
     // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
     var speakWord = "Hello";
 
     // STEP 4: Rewrite the 'speak' function such that it is attached to the
     // helloSpeaker object instead of being a standalone function.
     // See Lecture 52, part 2
-    var helloSpeaker = function (name) {
+    helloSpeaker.speak = function (name) {
         console.log(speakWord + " " + name);
     }
     window.helloSpeaker = helloSpeaker;
